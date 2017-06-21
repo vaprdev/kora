edbd6d7ccef82a8db090be43699c3edf3d60f099
Dax Ravi <d@ironbay.digital>
2017-06-21 12:36:04 -0400

    Updated socket handler

--- lib/kora/server/websocket.ex
+++ lib/kora/server/websocket.ex
@@ -11,7 +11,7 @@ defmodule Kora.Websocket do
 		:ok
 	end
 
-	def websocket_handle({:text, content}, req, state) do
+	def websocket_handle({:text, content}, state) do
 		%{
 			"key" => key,
 			"action" => action,
@@ -25,22 +25,22 @@ defmodule Kora.Websocket do
 		result = Map.put(result, :key, key)
 
 		json = Poison.encode!(result)
-		{:reply, {:text, json}, req, %{
+		{:reply, {:text, json}, %{
 			state |
 			data: data,
 		}}
 	end
 
-	def websocket_info(msg, req, state) do
+	def websocket_info(msg, state) do
 		case Kora.Command.trigger_info(msg, {:websocket, self()}, state.data) do
 			{:noreply, data} ->
-				{:ok, req, %{
+				{:ok, %{
 					state |
 					data: data
 				}}
 			{result, data} ->
 				json = Poison.encode!(result)
-				{:reply, {:text, json}, req, %{
+				{:reply, {:text, json}, %{
 					state |
 					data: data
 				}}
