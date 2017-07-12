9e33216acc422073f327fad25339c1433370935d
Dax Ravi <d@ironbay.digital>
2017-07-12 08:00:08 -0400

    Add it back

--- lib/kora/store/postgres.ex
+++ lib/kora/store/postgres.ex
@@ -22,7 +22,7 @@ defmodule Kora.Store.Postgres do
 			SELECT path, value
 			FROM kora
 			WHERE path <@ $1
-		""", [joined])
+		""" |> IO.inspect, [joined] |> IO.inspect)
 		|> Map.get(:rows)
 		|> Stream.map(fn [path, value] ->
 			splits =
