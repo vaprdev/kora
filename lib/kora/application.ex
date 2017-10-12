df14243a96d41ea394eb7d7fb1f14af180ba869d
Dax Ravi <d@ironbay.co>
2017-10-12 10:07:53 -0400

    Remove default postgres

--- lib/kora/application.ex
+++ lib/kora/application.ex
@@ -4,15 +4,7 @@ defmodule Kora.Application do
 	def start(_type, _args) do
 		import Supervisor.Spec, warn: false
 		children = [
-			worker(Registry, [[keys: :duplicate, name: Kora.Group]]),
-			Kora.Store.Postgres.child_spec([
-				hostname: "elb-ridehealth-8531.aptible.in",
-				database: "ridehealth",
-				username: "aptible",
-				password: "NIHFS1fSY5kTtH3GU1Y_04EO1Kt6ZGqe",
-				ssl: true,
-				name: :next,
-			]),
+			worker(Registry, [[keys: :duplicate, name: Kora.Group]])
 		]
 		opts = [strategy: :one_for_one, name: Kora.Supervisor]
 		Supervisor.start_link(children, opts)
