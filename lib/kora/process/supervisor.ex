51eabe0ec7f3cfbcd0f305f790edc5e92b960ec9
Dax Ravi <d@ironbay.co>
2017-10-12 16:10:31 -0400

    Restart crashed workers

--- lib/kora/process/supervisor.ex
+++ lib/kora/process/supervisor.ex
@@ -6,7 +6,7 @@ defmodule Kora.Process.Supervisor do
 	def init([module]) do
 		import Supervisor.Spec
 		children = [
-			worker(module, [], restart: :temporary)
+			worker(module, [], restart: :transient)
 		]
 		supervise(children, strategy: :simple_one_for_one)
 	end
