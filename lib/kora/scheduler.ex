2b5fa9d46e6e9fca879f93b55215f0a3dc494cfb
Alan Rice <mail@alanrice.me>
2017-11-14 14:28:36 -0500

    Fix scheduler compilation/warnings (#10)

--- lib/kora/scheduler.ex
+++ lib/kora/scheduler.ex
@@ -2,17 +2,17 @@ defmodule Kora.Scheduler do
         alias Kora.UUID
         use Kora.Worker
         
-        def handle_info(:resume, [key, timestamp | _rest], _state) do
-                Process.send_after(self(), :process, max(timestamp - :os.system_time(:millisecond), 0)        
-                %{:noreply, %{}}
+        def handle_info(:resume, [_key, timestamp | _rest], _state) do
+                Process.send_after(self(), :process, max(timestamp - :os.system_time(:millisecond), 0))
+                {:noreply, %{}}
         end
         
-        def handle_info(:process, [key, _timestamp, mod, fun, args], state) do 
+        def handle_info(:process, [_key, _timestamp, mod, fun, args], state) do 
                 apply(String.to_atom(mod), String.to_atom(fun), args)
                 {:stop, :shutdown, state}
         end
         
-        def handle_info(:cancel, [key | _rest], state) do 
+        def handle_info(:cancel, [_key | _rest], state) do 
                 {:stop, :shutdown, state} 
         end
         
